f6bc2689f741e925f8d3affab3de38c2dad6752d
AzathothYS <34173752+AzathothYS@users.noreply.github.com>
2020-09-07 23:05:47 +0200

    Simple 4 instructions processor

--- Minecraft Computer Breakdown/Instructions.h
+++ Minecraft Computer Breakdown/Instructions.h
@@ -3,25 +3,71 @@
 #include "data_types.h"
 
 
-#define _OP_DEF(name, opcode, instruction_class) struct name : public instruction_class<opcode> { using instruction_class::instruction_class; }
+#define _OP_DEF(name, code, instruction_class) struct name : public instruction_class { const U8 opcode = code; using instruction_class::instruction_class; }
 
 
 namespace ISA 
 {
 	namespace Resigters
 	{
-		U8 EAX = 1;
-		U8 ECX = 2;
-		U8 EDX = 3;
+		const U8 EAX = 0;
+		const U8 ECX = 1;
+		const U8 EDX = 2;
+		const U8 EBX = 3;
+		const U8 ESP = 4;
+		const U8 EBP = 5;
+		const U8 ESI = 6;
+		const U8 EDI = 7;
+
+		const U8 AX = 8;
+		const U8 CX = 9;
+		const U8 DX = 10;
+		const U8 BX = 11;
+		const U8 SP = 12;
+		const U8 BP = 13;
+		const U8 SI = 14;
+		const U8 DI = 15;
+
+		const U8 AH = 16;
+		const U8 CH = 17;
+		const U8 DH = 18;
+		const U8 BH = 19;
+		const U8 SPH = 20;
+		const U8 BPH = 21;
+		const U8 SIH = 22;
+		const U8 DIH = 23;
+
+		const U8 AL = 24;
+		const U8 CL = 25;
+		const U8 DL = 26;
+		const U8 BL = 27;
+		const U8 SPL = 28;
+		const U8 BPL = 29;
+		const U8 SIL = 30;
+		const U8 DIL = 31;
 	};
 
-	struct BaseInstruction {};
+	namespace Opcodes
+	{
+		const U8 ADD = 0x00;
+		const U8 JO = 0x70;
+		const U8 XCHG = 0x90;
+		const U8 MOV = 0xA0;
+		const U8 NOP = 0x90;
+	};
 
-	template<U8 opcode>
-	struct Instruction : public BaseInstruction {};
+	struct Instruction {
+		const U8 opcode = -1;
+	};
 
-	template<U8 opcode>
-	struct MemoryInstruction : public Instruction<opcode>
+	struct SomeInstruction : public Instruction 
+	{
+		SomeInstruction(U8 some_parameter) : some_parameter(some_parameter) {}
+
+		U8 some_parameter;
+	};
+
+	struct MemoryInstruction : public Instruction
 	{
 		MemoryInstruction(U8 source, U8 destination) : source(source), destination(destination) {}
 
@@ -29,8 +75,7 @@ namespace ISA
 		U8 destination;
 	};
 
-	template<U8 opcode>
-	struct ArithmeticInstruction : public Instruction<opcode>
+	struct ArithmeticInstruction : public Instruction
 	{
 		ArithmeticInstruction(U8 destination, U8 operand) : destination(destination), operand(operand) {}
 
@@ -38,8 +83,7 @@ namespace ISA
 		U8 operand;
 	};
 
-	template<U8 opcode>
-	struct JumpInstruction : public Instruction<opcode>
+	struct JumpInstruction : public Instruction
 	{
 		JumpInstruction(U32 address) : address(address) {}
 
@@ -47,18 +91,14 @@ namespace ISA
 	};
 
 
-	
 	_OP_DEF(ADD, 0x00, ArithmeticInstruction);
 
 	_OP_DEF(JO, 0x70, JumpInstruction); // FIXME: incorrect opcode
 		
 	_OP_DEF(XCHG, 0x90, MemoryInstruction);
 	_OP_DEF(MOV, 0xA0, MemoryInstruction);
+	
 
-	_OP_DEF(NOP, 0x90, Instruction);
-	
-	
-	
 	namespace Memory
 	{
 		using ISA::MOV;
--- Minecraft Computer Breakdown/registers.h
+++ Minecraft Computer Breakdown/registers.h
@@ -1,11 +1,146 @@
 #pragma once
 
+#include <memory>
+
 #include "data_types.h"
+
+
+struct Registers
+{
+	/*
+		General-Purpose registers
+	*/
+
+	U32 registers[8]{
+		// EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI
+		     0,   0,   0,   0,   0,   0,   0,   0
+	};
+
+	U32 read(const U8 register_id) const 
+	{
+		if (register_id < 8) {
+			return read_32(register_id);
+		}
+		else if (register_id < 16) {
+			return read_16(register_id);
+		}
+		else if (register_id < 24) {
+			return read_8_High(register_id);
+		}
+		else if (register_id < 32) {
+			return read_8_Low(register_id);
+		}
+		else {
+			return 0;
+		}
+	}
+
+	void write(const U8 register_id, const U32 new_value)
+	{
+		if (register_id < 8) {
+			write_32(register_id, new_value);
+		}
+		else if (register_id < 16) {
+			write_16(register_id, new_value);
+		}
+		else if (register_id < 24) {
+			write_8_High(register_id, new_value);
+		}
+		else if (register_id < 32) {
+			write_8_Low(register_id, new_value);
+		}
+	}
+
+	U32 read_32(const U8 register_id) const     { return registers[register_id % 8]; }
+	U16 read_16(const U8 register_id) const     { return registers[register_id % 8] & 0xFF'FF; }
+	U16 read_8_High(const U8 register_id) const { return (registers[register_id % 8] & 0xFF'00) >> 8; }
+	U16 read_8_Low(const U8 register_id) const  { return registers[register_id % 8] & 0xFF; }
+
+	void write_32(const U8 register_id, U32 new_value)    { registers[register_id % 8] = new_value; }
+	void write_16(const U8 register_id, U16 new_value)    { registers[register_id % 8] = (registers[register_id % 8] & 0xFF'FF'00'00) | new_value; }
+	void write_8_High(const U8 register_id, U8 new_value) { registers[register_id % 8] = (registers[register_id % 8] & 0xFF'FF'00'FF) | (new_value << 8); }
+	void write_8_Low(const U8 register_id, U8 new_value)  { registers[register_id % 8] = (registers[register_id % 8] & 0xFF'FF'FF'00) | new_value; }
+
+	/*
+		Instruction pointer
+	*/
+
+	U32 EIP = 0;
+	U32 read_EIP() const { return EIP; }
+	void write_EIP(U32 value) { EIP = value; }
+
+	/*
+		Flags
+	*/
+
+	U32 EFLAGS = 0b10; // bit 1 is always 1
+
+	U32 read_EFLAGS() const { return EFLAGS; }
+	U16 read_FLAGS() const { return EFLAGS & 0xFF'FF; }
+
+	bit flag_read_CF() const { return bool(EFLAGS & 0x001); }      // Carry Flag
+	bit flag_read_PF() const { return bool(EFLAGS & 0x004); }      // Parity Flag
+	bit flag_read_AF() const { return bool(EFLAGS & 0x010); }      // Adjust Flag
+	bit flag_read_ZF() const { return bool(EFLAGS & 0x040); }      // Zero Flag
+	bit flag_read_SF() const { return bool(EFLAGS & 0x080); }      // Sign Flag
+	bit flag_read_TF() const { return bool(EFLAGS & 0x100); }      // Trap Flag
+	bit flag_read_IF() const { return bool(EFLAGS & 0x200); }      // Interruption Flag
+	bit flag_read_DF() const { return bool(EFLAGS & 0x400); }      // Direction Flag
+	bit flag_read_OF() const { return bool(EFLAGS & 0x800); }      // Overflow Flag
+	bit flag_read_IOPL_L() const { return bool(EFLAGS & 0x1000); } // first IOPL bit
+	bit flag_read_IOPL_H() const { return bool(EFLAGS & 0x2000); } // second IOPL bit
+	bit flag_read_NT() const { return bool(EFLAGS & 0x4000); }     // Nested Task Flag
+	bit flag_read_RF() const { return bool(EFLAGS & 0x1'0000); }   // Resume Flag
+	bit flag_read_VM() const { return bool(EFLAGS & 0x2'0000); }   // Virtual 8086 Mode Flag
+	bit flag_read_AC() const { return bool(EFLAGS & 0x4'0000); }   // Alignment Check Flag
+	bit flag_read_VIF() const { return bool(EFLAGS & 0x8'0000); }  // Virtual Interruption Flag
+	bit flag_read_VIP() const { return bool(EFLAGS & 0x10'0000); } // Virtual Interrupt Pending Flag
+	bit flag_read_ID() const { return bool(EFLAGS & 0x20'0000); }  // Identification Flag
+
+	/* Carry Flag */ void flag_write_CF(bit value) { EFLAGS |= value * 0x001; }     // 
+	void flag_write_PF(bit value) { EFLAGS |= value * 0x004; }	   // Parity Flag
+	void flag_write_AF(bit value) { EFLAGS |= value * 0x010; }	   // Adjust Flag
+	void flag_write_ZF(bit value) { EFLAGS |= value * 0x040; }	   // Zero Flag
+	void flag_write_SF(bit value) { EFLAGS |= value * 0x080; }	   // Sign Flag
+	void flag_write_TF(bit value) { EFLAGS |= value * 0x100; }	   // Trap Flag
+	void flag_write_IF(bit value) { EFLAGS |= value * 0x200; }	   // Interruption Flag
+	void flag_write_DF(bit value) { EFLAGS |= value * 0x400; }	   // Direction Flag
+	void flag_write_OF(bit value) { EFLAGS |= value * 0x800; }	   // Overflow Flag
+	void flag_write_IOPL_L(bit value) { EFLAGS |= value * 0x1000; }// first IOPL bit
+	void flag_write_IOPL_H(bit value) { EFLAGS |= value * 0x2000; }// second IOPL bit
+	void flag_write_NT(bit value) { EFLAGS |= value * 0x4000; }	   // Nested Task Flag
+	void flag_write_RF(bit value) { EFLAGS |= value * 0x1'0000; }  // Resume Flag
+	void flag_write_VM(bit value) { EFLAGS |= value * 0x2'0000; }  // Virtual 8086 Mode Flag
+	void flag_write_AC(bit value) { EFLAGS |= value * 0x4'0000; }  // Alignment Check Flag
+	void flag_write_VIF(bit value) { EFLAGS |= value * 0x8'0000; } // Virtual Interruption Flag
+	void flag_write_VIP(bit value) { EFLAGS |= value * 0x10'0000; }// Virtual Interrupt Pending Flag
+	void flag_write_ID(bit value) { EFLAGS |= value * 0x20'0000; } // Identification Flag
+
+	/*
+		Utilities
+	*/
+
+	void reset_GPRs()
+	{
+		std::memset(registers, 0, 8);
+	}
+
+	void complete_reset()
+	{
+		reset_GPRs();
+
+		EIP = 0;
+		EFLAGS = 0b10;
+	}
+};
+
+
+
 
 /*
 	Container for reading / writing all x86 registers
 */
-struct Registers
+struct Registers__
 {
 	/*
 		General-Purpose registers
